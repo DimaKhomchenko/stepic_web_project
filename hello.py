e91dc8a0755c15f5616038fc14af1a96ab54428d
DimaKhomchenko <123@mail.net>
2020-04-18 21:57:24 +0700

    test

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 def app(environ, start_response):
 	start_response('200 OK', [('Content-Type', 'text/plain')])
 	body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
-	#eturn body
-	return ["Hello"]
+	return body
+
