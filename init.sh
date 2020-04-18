2752882b6e1379e180e64543d128b3b3bc51c194
DimaKhomchenko <123@mail.net>
2020-04-18 21:07:42 +0700

    test

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn/test
 sudo /etc/init.d/nginx restart
-sudo gunicorn -c /etc/gunicorn.d/test /home/box/web/etc/hello.py
+sudo gunicorn -c /etc/gunicorn.d/test /home/box/web/hello.py
